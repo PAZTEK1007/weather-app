66766c2bd53283cbef0b2a87acc254bc31a82eb7
Mathias Barbier <mathbarbier07@gmail.com>
2023-10-15 11:25:21 +0200

    update

---
+++ vite.config.js
@@ -0,0 +1,10 @@
+import { defineConfig } from "vite";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+// Rest of your Vite configuration
+
+export default defineConfig({
+  // Vite options
+});
